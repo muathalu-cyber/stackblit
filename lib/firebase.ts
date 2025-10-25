b5719fb1f147390c79dd69de8bfc3bfbd762724e
muathalu-cyber <muathalu@gmail.com>
2025-10-25 20:02:48 +0300

    asdsa

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from "firebase/database"
 import { doc, getFirestore, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTRzIOu6yN0Fiq6HGgtam6xc9MIsGKiE4",
-  authDomain: "tree-sa.firebaseapp.com",
-  databaseURL: "https://tree-sa-default-rtdb.firebaseio.com",
-  projectId: "tree-sa",
-  storageBucket: "tree-sa.firebasestorage.app",
-  messagingSenderId: "1028453914907",
-  appId: "1:1028453914907:web:31fd99fe3dcdb3b6fc5bbe",
-  measurementId: "G-9QR56T5899"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
