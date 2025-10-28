df4e9da60985434648850ea5631390a1fac10377
muathalu-cyber <muathalu@gmail.com>
2025-10-28 09:40:07 -0700

    sssss

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,14 @@ import { getDatabase } from "firebase/database"
 import { doc, getFirestore, setDoc } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGXGWc8wON-OL0mEi2vX_B5K7PytlHjfw",
-  authDomain: "ah-moror.firebaseapp.com",
-  projectId: "ah-moror",
-  storageBucket: "ah-moror.firebasestorage.app",
-  messagingSenderId: "869838548515",
-  appId: "1:869838548515:web:9d1bb5c87d96d2f2d74b96",
-  measurementId: "G-4VGVLNQK1L"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
